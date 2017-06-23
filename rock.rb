c8bb0318118ba65f7ae6454e845ab65ff27b1361
Ana Maria Sosa <anamariasosam@gmail.com>
2017-06-23 15:42:10 -0500

    add a new class Song

--- rock.rb
+++ rock.rb
@@ -2,64 +2,73 @@ require 'HTTParty'
 require 'json'
 require 'launchy'
 
+# Song Class
+class Song
+  attr_reader :name, :musixmatch_id, :spotify_id, :words
+  def initialize(name, musixmatch_id, spotify_id, words)
+    @name = name
+    @musixmatch_id = musixmatch_id
+    @spotify_id = spotify_id
+    @words = words
+    @lyric = lyric
+  end
+
+  def lyric
+    url = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get'
+    query = {
+      'track_id' => @musixmatch_id,
+      'apikey' => 'c4caa5ba22a90e53f85590fdecf2347d'
+    }
+    response = HTTParty.get(url, query: query)
+    data = JSON.parse(response.body)
+
+    lyric = data['message']['body']['lyrics']['lyrics_body']
+
+    @words.each { |e| lyric[e] = '________' }
+
+    lyric
+  end
+
+  def play
+    Launchy.open("https://open.spotify.com/track/#{@spotify_id}")
+  end
+end
+
+s1 = Song.new('Wild Thoughts',
+              '130727382',
+              '4IIUaKqGMElZ3rGtuvYlNc',
+              %w[baby wasted thoughts hope])
+
+s2 = Song.new('Slow Hands',
+              '129347748',
+              '167NczpNbRF7oWakJaY3Hh',
+              %w[said thinking hands leaving])
+
+s3 = Song.new('Im the One',
+              '130727383',
+              '72Q0FQQo32KJloivv5xge2',
+              %w[money sick only promise])
+
+s4 = Song.new('Bad Liar',
+              '129971067',
+              '1sCxVKWImDZSZKvG0U9B23',
+              %w[street someone rent watch])
+
+songs = { '1' => s1, '2' => s2, '3' => s3, '4' => s4 }
+
 puts 'Which song do you want?
 (1) Wild Thoughts
 (2) Slow Hands
 (3) Im the One
 (4) Bad Liar'
 
-while option = gets.chomp.to_i
-  case option
-  when 1 then
-    track_id = '130727382'
-    spotify_id = '4IIUaKqGMElZ3rGtuvYlNc'
-    break
-  when 2 then
-    track_id = '129347748'
-    spotify_id = '167NczpNbRF7oWakJaY3Hh'
-    break
-  when 3 then
-    track_id = '130727383'
-    spotify_id = '72Q0FQQo32KJloivv5xge2'
-    break
-  when 4 then
-    track_id = '129971067'
-    spotify_id = '1sCxVKWImDZSZKvG0U9B23'
-    break
-  else
-    puts 'Please select a song'
-    print '>'
-  end
-end
+song = gets.chomp
 
-query = {
-  'track_id' => track_id,
-  'apikey' => 'c4caa5ba22a90e53f85590fdecf2347d'
-}
+puts "**************************************************
+          SONG NAME : #{songs[song].name}
+**************************************************"
 
-url = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get'
-response = HTTParty.get(url, query: query)
-Launchy.open("https://open.spotify.com/track/#{spotify_id}")
-
-data = JSON.parse(response.body)
-lyric = data['message']['body']['lyrics']['lyrics_body']
-
-def add_line(array, lyric)
-  array.each { |e| lyric[e] = '________' }
-end
-
-case option
-when 1 then add_line(%w[baby wasted thoughts hope], lyric)
-when 2 then add_line(%w[said thinking hands leaving], lyric)
-when 3 then add_line(%w[money sick only promise], lyric)
-when 4 then add_line(%w[street someone rent watch], lyric)
-end
-
-puts '**************************************************'
-puts '                      LYRIC                       '
-puts '**************************************************'
-
-puts lyric
+puts songs[song].lyric
 
 puts '**************************************************'
 puts '                   LYRIC END                       '
@@ -67,8 +76,7 @@ puts '**************************************************'
 
 puts 'Write the words separated by commas'
 words = gets.chomp.split(',')
-
-solution = %w[baby wasted thoughts hope]
+solution = songs[song].words
 guessed = []
 
 solution.each_with_index do |word, index|
